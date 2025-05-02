fc411b431e4951b82a6b6a600759a383f1c02551
iKazai <91919794+iKazai@users.noreply.github.com>
2025-05-03 01:39:26 +0200

    installe gsap et tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,25 @@
 import './App.css'
 
-function App() {
+import { gsap } from "gsap";
+import { useGSAP } from "@gsap/react";
 
+gsap.registerPlugin(useGSAP);
+
+function App() {
 
   return (
     <>
-
+      <div className="hero bg-base-200 min-h-screen">
+        <div className="hero-content text-center">
+          <div className="max-w-md">
+            <h1 className="text-5xl font-bold">WIP</h1>
+            <p className="py-6">
+              Rien à voir ici...
+            </p>
+            <button className="btn btn-primary">Quitter le site</button>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
