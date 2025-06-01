f6c9f171ab5537f86e815aa892199295c1a96510
iKazai <91919794+iKazai@users.noreply.github.com>
2025-06-01 14:56:41 +0200

    save 01/06/2025

--- src/Table.tsx
+++ src/Table.tsx
@@ -4,9 +4,9 @@ export default function TableOfContents() {
             <div className="TableOfContents bg-base-100 h-screen flex flex-col justify-center items-start p-6">
                 <h2 className="text-5xl font-bold mb-8">Table of Contents</h2>
                 <ul className="text-3xl space-y-4">
-                    <li><a href="#section1" className="hover:text-[#b5dcff] transition-colors duration-300">Section 1</a></li>
-                    <li><a href="#section2" className="hover:text-[#b5dcff] transition-colors duration-300">Section 2</a></li>
-                    <li><a href="#section3" className="hover:text-[#b5dcff] transition-colors duration-300">Section 3</a></li>
+                    <li><a href="#projects" className="hover:text-[#b5dcff] transition-colors duration-300">Projects</a></li>
+                    <li><a href="#pictures" className="hover:text-[#b5dcff] transition-colors duration-300">Pictures</a></li>
+                    <li><a href="#about" className="hover:text-[#b5dcff] transition-colors duration-300">About</a></li>
                 </ul>
             </div>
         </>
